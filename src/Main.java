d95215526d098233a0a06d49fa346b46e6e63677
alex <darkeitor6@outlook.com>
2024-08-13 14:51:11 -0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello and welcome!");
+
+        Scanner scanner = new Scanner(System.in);
+        int num;
+        num = scanner.nextInt();
+        int numb = genNum();
+        System.out.println(numb);
+        System.out.println(num);
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+
+    private static int genNum() {
+        return (int) (Math.random() * 10);
+    }
+}
